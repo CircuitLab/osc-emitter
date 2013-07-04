40f3e90edd859dfddec30f110d5fb8d5e6696242
nulltask <nulltask@gmail.com>
2013-07-04 20:46:14 +0900

    fix test for readability

--- test/index.js
+++ test/index.js
@@ -16,43 +16,49 @@ describe('osc-emitter', function() {
 
   describe('OscEmitter', function() {
     var emitter = new OscEmitter();
-  
+
     it('#add()', function() {
       emitter.add('localhost', 31000);
       emitter.add('localhost', 31001);
+
       assert(2 === emitter._receivers.length);
     });
-  
+
     it('#remove()', function() {
       emitter.remove('localhost', 31000);
+
       assert(1 === emitter._receivers.length);
     });
-  
+
     it('#emit(number, number)', function(done) {
       sock.once('message', function(buf) {
         var message = osc.fromBuffer(buf);
+
         assert('/foo' === message.address);
         assert(2 === message.args.length);
         assert(1 === message.args[0].value);
         assert(2 === message.args[1].value);
+
         done();
       });
-  
+
       setImmediate(function() {
         emitter.emit('/foo', 1, 2);
       });
     });
-  
+
     it('#emit(string, string)', function(done) {
       sock.once('message', function(buf) {
         var message = osc.fromBuffer(buf);
+
         assert('/bar' === message.address);
         assert(2 === message.args.length);
         assert('hello' === message.args[0].value);
         assert('world' === message.args[1].value);
+
         done();
       });
-  
+
       setImmediate(function() {
         emitter.emit('/bar', 'hello', 'world');
       });
